b1cdeea7f9008c23fdaf87228e36154897ddc096
HerrKarotte <dvp03ne@yandex.ru>
2025-05-05 20:13:59 +0300

    DB: add poster_preview field

--- app/src/main/java/com/example/filmfinder/db/MovieTable.kt
+++ app/src/main/java/com/example/filmfinder/db/MovieTable.kt
@@ -20,10 +20,15 @@ data class MovieEntity(
     @ColumnInfo("genres") val genres: String?,
     @ColumnInfo("posterUrl") val posterUrl: String?,
     @ColumnInfo("posterUrlPreview") val posterUrlPreview: String?,
-    @ColumnInfo("posterBytes") val posterBytes: ByteArray?
+    @ColumnInfo("posterBytes") val posterBytes: ByteArray?,
+    @ColumnInfo("previewPosterBytes") val previewPosterBytes: ByteArray?
 ) {
     companion object {
-        fun fromMovieItem(movie: MovieItem, posterBytes: ByteArray? = null): MovieEntity {
+        fun fromMovieItem(
+            movie: MovieItem,
+            posterBytes: ByteArray? = null,
+            previewPosterBytes: ByteArray? = null
+        ): MovieEntity {
             val countriesList = movie.countries.let { Gson().toJson(it) }
             val genresList = movie.genres.let { Gson().toJson(it) }
             return MovieEntity(
@@ -36,7 +41,8 @@ data class MovieEntity(
                 countries = countriesList,
                 posterUrl = movie.posterUrl,
                 posterUrlPreview = movie.posterUrlPreview,
-                posterBytes = posterBytes
+                posterBytes = posterBytes,
+                previewPosterBytes = previewPosterBytes
             )
         }
     }
@@ -45,8 +51,8 @@ data class MovieEntity(
         val type = object : TypeToken<List<Countries>>() {}.type
         val countriesList: List<Countries> =
             countries?.let { Gson().fromJson(it, type) } ?: emptyList()
-        val genretype = object : TypeToken<List<Genres>>() {}.type
-        val genresList: List<Genres> = genres?.let { Gson().fromJson(it, genretype) } ?: emptyList()
+        val genreType = object : TypeToken<List<Genres>>() {}.type
+        val genresList: List<Genres> = genres?.let { Gson().fromJson(it, genreType) } ?: emptyList()
 
         return MovieItem.Movie(
             id = this.movieId,
@@ -59,5 +65,29 @@ data class MovieEntity(
             posterUrl = posterUrl,
             posterUrlPreview = posterUrlPreview
         )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MovieEntity
+
+        if (posterBytes != null) {
+            if (other.posterBytes == null) return false
+            if (!posterBytes.contentEquals(other.posterBytes)) return false
+        } else if (other.posterBytes != null) return false
+        if (previewPosterBytes != null) {
+            if (other.previewPosterBytes == null) return false
+            if (!previewPosterBytes.contentEquals(other.previewPosterBytes)) return false
+        } else if (other.previewPosterBytes != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = posterBytes?.contentHashCode() ?: 0
+        result = 31 * result + (previewPosterBytes?.contentHashCode() ?: 0)
+        return result
     }
 }
