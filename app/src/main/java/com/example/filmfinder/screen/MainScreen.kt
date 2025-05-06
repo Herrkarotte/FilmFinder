65edf694c7190eec9d7a50014d327c6d2db336d4
HerrKarotte <dvp03ne@yandex.ru>
2025-05-06 23:53:09 +0300

    Add pullToRefresh functionality

--- app/src/main/java/com/example/filmfinder/screen/MainScreen.kt
+++ app/src/main/java/com/example/filmfinder/screen/MainScreen.kt
@@ -5,9 +5,11 @@ package com.example.filmfinder.screen
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,6 +17,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Search
@@ -30,11 +34,14 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -64,6 +71,7 @@ fun MainScreen(navController: NavController) {
     val focusManager = LocalFocusManager.current
     val snackbarHostState = remember { SnackbarHostState() }
     val snackbarMessage = viewModel.snackBarMessage.value
+    var isInitialLoad by remember { mutableStateOf(true) }
 
     LaunchedEffect(snackbarMessage) {
         snackbarMessage?.let { message ->
@@ -97,27 +105,46 @@ fun MainScreen(navController: NavController) {
                 }
             })
     }) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
         ) {
-            when {
-                films.loadState.refresh is LoadState.Loading -> {
-                    CircularProgressIndicator()
+            PullToRefreshBox(
+                isRefreshing = films.loadState.refresh is LoadState.Loading && !isInitialLoad,
+                onRefresh = {
+                    isInitialLoad = false
+                    films.refresh()
                 }
+            ) {
+                when {
+                    films.loadState.refresh is LoadState.Loading -> {
+                        Box(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                        }
+                    }
 
-                films.loadState.refresh is LoadState.Error -> {
-                    ErrorMessage(
-                        error = (films.loadState.refresh as LoadState.Error).error.message
-                            ?: "Неизвестная ошибка"
-                    )
-                }
+                    films.loadState.refresh is LoadState.Error -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            ErrorMessage(
+                                error = (films.loadState.refresh as LoadState.Error).error.message
+                                    ?: "Неизвестная ошибка"
+                            )
+                        }
+                    }
 
-                else -> {
-                    FilmsList(
-                        films = films,
-                        navController,
-                        addToFavor = { id -> viewModel.toggleFavor(id) })
+                    else -> {
+                        FilmsList(
+                            films = films,
+                            navController,
+                            addToFavor = { id -> viewModel.toggleFavor(id) })
+                    }
                 }
             }
         }
@@ -127,7 +154,7 @@ fun MainScreen(navController: NavController) {
 
 @Composable
 fun FilmsList(
-    films: LazyPagingItems<MovieItem>, navController: NavController, addToFavor: (Int) -> Unit
+    films: LazyPagingItems<MovieItem>, navController: NavController, addToFavor: (Int) -> Unit,
 ) {
     LazyColumn {
         items(films.itemCount) { index ->
@@ -137,6 +164,7 @@ fun FilmsList(
         }
     }
 }
+
 
 @Composable
 fun FilmsItem(
